357aae561ea6ae80b73609f9d399ec497ac9d3f6
MatteoOliva <oliva.matteo98@outlook.com>
2024-04-04 15:35:29 +0200

    tabella projects stampata

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,7 +3,40 @@
 
 <div class="container">
 <h1>Lista projects</h1>
-@dump($projects)
+
+<table class="table">
+
+<thead>
+<tr>
+    <th>titolo</th>
+    <th>slug</th>
+    <th>content</th>
+</tr>
+</thead>
+<tbody>
+@forelse($projects as $project)
+
+<tr>
+    <td>{{$project->title}}</td>
+    <td>{{$project->slug}}</td>
+    <td>{{$project->content}}</td>
+    
+</tr>
+@empty
+<tr>
+    <td colspan="100%">
+        <i>nessun risultato</i>
+    </td>
+</tr>
+
+@endforelse
+
+</tbody>
+
+
+</table>
+
+
 
 
 </div>
